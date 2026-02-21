33f3498016a6bcf6a2d7f0a1edb6ad4e6912bc3a
Ruslan <info@mirasov.dev>
2026-02-21 17:41:30 +0100

    create generator_numbers and sum_profit functions

--- task-2/main.py
+++ task-2/main.py
@@ -1,1 +1,28 @@
-print("Start")
+import re
+from typing import Callable
+
+# Генерує всі дійсні числа з тексту (включно з від'ємними).
+def generator_numbers(text: str):
+    pattern = r"-?\d+(?:\.\d+)?"
+
+    for number in re.findall(pattern, text):
+        yield float(number)
+
+# Обчислює загальну суму чисел.
+def sum_profit(text: str, func: Callable):
+    total = 0
+
+    for number in func(text):
+        total += number
+
+    return total
+
+# Приклади використання:
+text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+text_with_negative_numbers = "Загальний дохід 1000 доларів + премія 100 та сплата штрафу -50 доларів. Податки 0 доларів"
+
+total_income = sum_profit(text, generator_numbers)
+total_income_with_negative_numbers = sum_profit(text_with_negative_numbers, generator_numbers)
+
+print(f"Загальний дохід: {total_income}")
+print(f"Загальний дохід: {total_income_with_negative_numbers}")
